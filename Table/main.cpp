b432f717cf27bc9ecd678b20db47eb3881930754
Juicy-and <131388626+Juicy-and@users.noreply.github.com>
2023-08-30 22:36:32 +0300

    Update main.cpp

--- Table/main.cpp
+++ Table/main.cpp
@@ -6,36 +6,52 @@ private:
 	T **mass;
 	int x, y;
 public:
+
+	Table(const Table&) {}
+	void Table::operator=(const Table&) {}
+	
+
 	Table(int a, int b): x(a), y(b) 
 	{
 		mass = new T * [a];
 		for (int i = 0; i < a; i++)
 			mass[i] = new T [b];
 	}
+
 	T* operator[](int a)
 	{
 		return mass[a];
 	}
+
 	const T* operator[](int a) const
 	{
 		return mass[a];
 	}
+
 	T* operator=(int a)
 	{
 		return mass = a;
 	}
+
+
 	~Table() 
 	{
 		for (int i = 0; i < x; i++)
 			delete mass[i];
 		delete[] mass;
 	}
+	
 };
-
 
 int main() {
 	auto test = Table<int>(2, 3);
 	test[0][0] = 4;
-	std::cout << test[0][0]; // âûâîäèò 4
+	std::cout << test[0][0]; // Ğ²Ñ‹Ğ²Ğ¾Ğ´Ğ¸Ñ‚ 4
+
+	Table<int> t1(2, 3);
+	Table<int> t2(2, 3);
+
+	t1 = t2;
+
 	return 0;
 }
